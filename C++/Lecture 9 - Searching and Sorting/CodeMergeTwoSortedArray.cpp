a53203cbb8690b97cbfa6b4b45fb0f43ea98bc4c
Ankan Das <ankan4talk@gmail.com>
2022-05-28 11:16:44 +0530

    You have been given two sorted arrays/lists(ARR1 and ARR2) of size N and M respectively, merge them into a third array/list such that the third array is also sorted.

---
+++ C++/Lecture 9 - Searching and Sorting/CodeMergeTwoSortedArray.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+void merge(int *arr1, int size1, int *arr2, int size2, int *ans)
+{
+    /* Different, unclear approach
+    if (size1 == 0)
+    {
+        for (int i = 0; i < size2; i++)
+            ans[i] = arr2[i];
+    }
+    else if (size2 == 0)
+    {
+        for (int i = 0; i < size1; i++)
+            ans[i] = arr1[i];
+    }
+    else
+    {
+        for (int i = 0, j = 0, k = 0; k < size1+size2; k++)
+        {
+            if (i == size1)
+            {
+                ans[k] = arr2[j];
+                j++;
+            }
+            else if (j == size2)
+            {
+                ans[k] = arr1[i];
+                i++;
+            }
+            else if (arr1[i] <= arr2[j])
+            {
+                ans[k] = arr1[i];
+                i++;
+            }
+            else if (arr1[i] > arr2[j])
+            {
+                ans[k] = arr2[j];
+                j++;
+            }
+        }
+    }*/
+    // Definite and clear approach 
+    int i =0, j=0, k=0;
+    while(i<size1 && j<size2)
+    {
+        if (arr1[i] < arr2[j])
+            ans[k++] = arr1[i++];
+        else
+            ans[k++] = arr2[j++];
+    }
+    while (i < size1)
+        ans[k++] = arr1[i++];
+    
+    while (j < size2)
+        ans[k++] = arr2[j++];
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+		int size1;
+		cin >> size1;
+
+		int *arr1 = new int[size1];
+
+		for (int i = 0; i < size1; i++)
+		{
+			cin >> arr1[i];
+		}
+
+		int size2;
+		cin >> size2;
+
+		int *arr2 = new int[size2];
+
+		for (int i = 0; i < size2; i++)
+		{
+			cin >> arr2[i];
+		}
+
+		int *ans = new int[size1 + size2];
+
+		merge(arr1, size1, arr2, size2, ans);
+
+		for (int i = 0; i < size1 + size2; i++)
+		{
+			cout << ans[i] << " ";
+		}
+
+		cout << endl;
+		delete[] arr1;
+		delete[] arr2;
+		delete[] ans;
+	}
+}
