0a0756baedf7c16fd0425586a3f051c27b77edcc
Ankan Das <ankan4talk@gmail.com>
2022-06-17 00:00:26 +0530

    Given an integer array A of size n. Find and print all the leaders present in the input array. An array element A[i] is called Leader, if all the elements following it (i.e. present at its right) are less than or equal to A[i]. Print all the leader elements separated by space and in the same order they are present in the input array.

--- C++/Test 2/LeadersInArray.cpp
+++ C++/Test 2/LeadersInArray.cpp
@@ -38,3 +38,36 @@ int main()
 	}
 	Leaders(arr,len);
 }
+#include<iostream>
+#include<climits>
+using namespace std;
+void Leaders(int* arr,int len)
+{
+	int largest = -99999999, j = 0;
+    int *leaders = new int[len];
+    for (int i = len - 1; i >= 0; i--)
+    {
+        if (arr[i] >= largest)
+        {
+            leaders[j] = arr[i];
+            j++;
+            largest = arr[i];
+        }
+    }
+    for (int i = j - 1; i >= 0; i--)
+    {
+        cout << leaders[i] << " ";
+    }
+}
+int main()
+{
+    int len;
+    cin>>len;
+	int *arr = new int[len + 1];
+	
+	for(int i=0;i<len;i++)
+	{
+		cin>>arr[i];
+	}
+	Leaders(arr,len);
+}
