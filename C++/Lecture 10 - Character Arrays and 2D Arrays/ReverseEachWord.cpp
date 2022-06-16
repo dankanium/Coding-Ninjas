36da44d4382f7f5e035d9dc0fc29d84bc2eef44a
Ankan Das <ankan4talk@gmail.com>
2022-06-17 00:03:47 +0530

    Aadil has been provided with a sentence in the form of a string as a function parameter. The task is to implement a function so as to print the sentence such that each word in the sentence is reversed. Example: Input Sentence: "Hello, I am Aadil!" The expected output will print, ",olleH I ma !lidaA".

--- C++/Lecture 10 - Character Arrays and 2D Arrays/ReverseEachWord.cpp
+++ C++/Lecture 10 - Character Arrays and 2D Arrays/ReverseEachWord.cpp
@@ -2,25 +2,28 @@
 #include <cstring>
 using namespace std;
 
-#include<cstring>
-void removeConsecutiveDuplicates(char input[]) {
-    int len = strlen(input);
-    int current = 1, next = 1;
-    char lastchar = input[0];
-    while(next < len) {
-        if(input[next] == lastchar) {
-            next++;
-        }
-        else
+void reverseWord(char input[], int s, int e)
+{
+    for (int i = s, j = e; i <= j; i++, j--)
+    {
+        char temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+}
+void reverseEachWord(char input[]) {
+    // Write your code here
+    int i = 0, j = 1;
+    while (j <= strlen(input))
+    {
+        if (input[j] == ' ' or input[j] == '\0')
         {
-            input[current] = input[next];
-            lastchar = input[next];
-            next++;
-            current++;
+            reverseWord(input, i, --j);
+            i = j + 2;
+            j = i + 1;
         }
-        if(next == len)
-        {
-            input[current] = '\0';
+        else {
+            j++;
         }
     }
 }
